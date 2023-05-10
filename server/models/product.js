a0e16f9efe08e86d19aa522d0675e0707c5d647f
yotak <tech.yuje@gmail.com>
2023-05-10 18:45:29 +0900

    Update product.js

--- server/models/product.js
+++ server/models/product.js
@@ -15,6 +15,8 @@ module.exports = (sequelize, Sequelize) => {
     {
       tableName: "product",
       timestamp: false,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
     }
   );
   return product;
