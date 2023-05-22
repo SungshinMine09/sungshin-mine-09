b8cff192e0903ad443ef35ad82a2fcbc1594f699
dahyun <70197115+dahyun-kim01@users.noreply.github.com>
2023-05-22 12:01:33 +0900

    feat: 홈페이지 [NEW 공동구매] 공구방리스트
    [NEW 공동구매에 CreatedAt DESC 순으로 정렬해서 공구방 리스트 2개 불러오기

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -1,5 +1,22 @@
+const db = require("../models/index"),
+  CoBuyRoom = db.cobuying_room;
+
 module.exports = {
-  index: (req, res) => {
-    res.render("home/index");
+  index: async (req, res) => {
+    try {
+      newGonggus = await CoBuyRoom.findAll({
+        order: [ ['createdAt', 'DESC'] ],
+        limit: 2
+      });
+      hotGonggus = await CoBuyRoom.findAll({
+        order: [ ['createdAt', 'DESC' ] ],
+        limit: 2
+      })
+      res.render("home/index", {newGonggus: newGonggus});
+    } catch(error) {
+      res.status(500).send({
+        message: err.message
+      });
+    }
   },
 };
