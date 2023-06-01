47d869b60df349334c87222bba88342c8e66e0ce
s5763305@naver.com <s5763305@naver.com>
2023-06-01 19:49:47 +0900

    feat: 회원 식별

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const notification = require("../models/notification");
+const jwt = require("jsonwebtoken");
 
 const db=require("../models/index"),
     Notification = db.notification;
@@ -24,9 +25,13 @@ module.exports = {
         notificationsJoinCobuyingRooms1 = await db.sequelize.query(
           'SELECT a.id as notificationId, a.receiver_id, a.cobuying_room_id, a.content, a.read_or_not, a.type2, a.url, a.createdAt, a.updatedAt, b.title, b.state, b.host_id FROM notifications as a JOIN cobuying_room as b ON a.`cobuying_room_id`=b.`id` WHERE receiver_id=2 or host_id=2 ORDER BY a.id  DESC;'
         );
+        const userToken = req.cookies['userToken'];
+        let decodedToken = jwt.verify(userToken,secretObj.secret);
         notificationsJoinCobuyingRoom1 = notificationsJoinCobuyingRooms1[0];
-        console.log(notificationsJoinCobuyingRoom1);
-        res.render("user/alarmPage", {notifications: notificationsJoinCobuyingRoom1});
+        // console.log(notificationsJoinCobuyingRoom1);
+        if(decodedToken) {
+          res.render("user/alarmPage", {notifications: notificationsJoinCobuyingRoom1, userID: decodedToken.db_id});
+        }
      } catch(error) {
        console.log(error);
      }  
@@ -36,7 +41,12 @@ module.exports = {
         notificationsJoinCobuyingRooms2 = await db.sequelize.query(
           "SELECT * FROM notifications as a JOIN cobuying_room as b ON a.`cobuying_room_id`=b.`id` WHERE (receiver_id=2 or host_id=2) and type2 != 'chat' ORDER BY a.id DESC;"
         );
+        const userToken = req.cookies['userToken'];
+        let decodedToken = jwt.verify(userToken,secretObj.secret);
         notificationsJoinCobuyingRoom2 = notificationsJoinCobuyingRooms2[0];
+        if(decodedToken) {
+          res.render("user/alarmPage", {notifications: notificationsJoinCobuyingRoom1, userID: decodedToken.db_id});
+        }
         res.render("user/coBuyRoomAlarm", {coBuyRoomNotifications: notificationsJoinCobuyingRoom2});
       } catch(error) {
         console.log(error);
@@ -47,7 +57,12 @@ module.exports = {
         notificationsJoinCobuyingRooms3 = await db.sequelize.query(
           "SELECT * FROM notifications as a JOIN cobuying_room as b ON a.`cobuying_room_id`=b.`id` WHERE (receiver_id=2 or host_id=2) and type2 = 'chat' ORDER BY a.id DESC;"
         );
+        const userToken = req.cookies['userToken'];
+        let decodedToken = jwt.verify(userToken,secretObj.secret);
         notificationsJoinCobuyingRoom3 = notificationsJoinCobuyingRooms3[0];
+        if(decodedToken) {
+          res.render("user/alarmPage", {notifications: notificationsJoinCobuyingRoom1, userID: decodedToken.db_id});
+        }
         res.render("user/chattingAlarm", {chattingNotifications: notificationsJoinCobuyingRoom3});
       } catch(error) {
         console.log(error);
